b4a1520efec926538edf48ea21f47cabf56e7e32
yrl <693855026@qq.com>
2023-12-13 16:04:20 +0800

    问题修改

---
+++ index copy.js
@@ -0,0 +1,10 @@
+// index.js
+import { crypto } from './utils/crypto'
+import { checkPermission } from './utils/permission'
+import { sso } from './utils/sso'
+
+export default {
+  crypto,
+  checkPermission,
+  sso
+}
